2e321fc22ecbf3ab959f7a79c9e632d9933305bc
koldun <eg.sharypov@yandex.ru>
2023-12-21 21:37:51 +0300

    gpt_wrapper

--- src/app.py
+++ src/app.py
@@ -1,38 +1,22 @@
 from dotenv import load_dotenv
 import os
-import requests
+from gpt_wrapper import GPTWrapper
 
 load_dotenv()
 
 yc_folder_id = os.getenv("YC_FOLDER_ID")
 yc_iam_token = os.getenv("YC_IAM_TOKEN")
 
-print("folder_id: ", yc_folder_id)
-print("IAM Token: ", yc_iam_token)
 
-req_url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
 
-req_payload = {
-    "modelUri": f"gpt://{yc_folder_id}/yandexgpt-lite",
-    "completionOptions": {
-        "stream": False,
-        "temperature": 0.6,
-        "maxTokens": "2000"
-    },
-    "messages": [{
+gpt = GPTWrapper(yc_folder_id, yc_iam_token)
+print(gpt.prompt([
+    {
         "role": "system",
-        "text": "Напиши конспект по истории"
-    }, {
+        "text": "Напиши краткий конспект по истории"
+    },
+    {
         "role": "user",
         "text": "Барокко"
-    }]
-}
-
-req_headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {yc_iam_token}",
-    "x-folder-id": yc_folder_id
-}
-
-res = requests.post(req_url, json=req_payload, headers=req_headers)
-print(res.text)
+    }
+]))
