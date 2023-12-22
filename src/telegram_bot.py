3188427548edac4492b3baa1b513a2cccd9509d1
koldun <eg.sharypov@yandex.ru>
2023-12-22 23:56:43 +0300

    basic quiz structure

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,6 +1,13 @@
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 from konspekti_engine import KonspektiEngine
+from enum import Enum
+from quiz import Quiz
+
+
+class BotState(Enum):
+    KONSPEKTI = 1
+    QUIZ = 2
 
 
 start_msg = """Это бот, который генерирует конспекты по истории
@@ -8,20 +15,48 @@ start_msg = """Это бот, который генерирует конспек
 
 
 class TelegramBot:
+    state = BotState.KONSPEKTI
+    quiz = None
+
     def __init__(self, token, gpt):
         self.app = Application.builder().token(token).build()
         self.app.add_handler(CommandHandler("start", self.start))
+        self.app.add_handler(CommandHandler("quiz", self.quiz))
         self.app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_msg))
+        self.gpt = gpt
         self.konspekti_engine = KonspektiEngine(gpt)
         self.app.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
     async def handle_msg(self, update, context):
         msg = update.message.text
-        answer = await self.konspekti_engine.gen_konspekt(msg)
+        if self.state == BotState.KONSPEKTI:
+            answer = await self.konspekti_engine.gen_konspekt(msg)
+            await update.message.reply_text(answer.replace("**", "*"), parse_mode="markdown")
+        elif self.state == BotState.QUIZ:
+            is_correct, next_question = self.quiz.answer(msg)
 
-        await update.message.reply_text(answer.replace("**", "*"), parse_mode="markdown")
+            if is_correct:
+                await update.message.reply_text(f"Правильно")
+            else:
+                await update.message.reply_text(f"Неправильно")
+
+            await update.message.reply_text(f"Ваши баллы: {self.quiz.points}")
+
+            if next_question is None:
+                await update.message.reply_text("Конец")
+                self.state = BotState.KONSPEKTI
+            else:
+                await update.message.reply_text(next_question.text)
 
 
     async def start(self, update, context):
         await update.message.reply_text(start_msg, parse_mode="markdown")
+
+
+    async def quiz(self, update, context):
+        await update.message.reply_text("Начало квиза")
+        self.state = BotState.QUIZ
+        self.quiz = Quiz(self.gpt, "Тест егэ по дота 2")
+        first_question = self.quiz.begin()
+        await update.message.reply_text(first_question)
