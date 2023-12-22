8faa23304ed8813b3fb5d64475fa599c5e52d699
koldun <eg.sharypov@yandex.ru>
2023-12-22 23:01:11 +0300

    split hist_engine into konspekti_engine and topic_classifier

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,23 +1,27 @@
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-from hist_engine import HistEngine
+from konspekti_engine import KonspektiEngine
+
 
 start_msg = """Это бот, который генерирует конспекты по истории
 Просто введите тему билета"""
+
 
 class TelegramBot:
     def __init__(self, token, gpt):
         self.app = Application.builder().token(token).build()
         self.app.add_handler(CommandHandler("start", self.start))
         self.app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_msg))
-        self.hist_engine = HistEngine(gpt)
+        self.konspekti_engine = KonspektiEngine(gpt)
         self.app.run_polling(allowed_updates=Update.ALL_TYPES)
+
 
     async def handle_msg(self, update, context):
         msg = update.message.text
-        answer = await self.hist_engine.gen_konspekti(msg)
+        answer = await self.konspekti_engine.gen_konspekt(msg)
 
         await update.message.reply_text(answer.replace("**", "*"), parse_mode="markdown")
+
 
     async def start(self, update, context):
         await update.message.reply_text(start_msg, parse_mode="markdown")
