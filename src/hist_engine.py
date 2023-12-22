74341b81c5726989fa3bf7a72c037194f562447e
koldun <eg.sharypov@yandex.ru>
2023-12-22 19:20:24 +0300

    moved some logic to hist_engine.py

---
+++ src/hist_engine.py
@@ -0,0 +1,31 @@
+class HistEngine():
+    def __init__(self, gpt):
+        self.gpt = gpt
+
+    async def gen_konspekti(self, prompt):
+        answer = await self.gpt.async_prompt([
+            {
+                "role": "system",
+                "text": f"Ты хорошо знаешь историю, говоришь только про историю."
+            },
+            {
+                "role": "user",
+                "text": f"Сгенерируй краткий конспект по истории по теме {prompt}. Назови основные даты, деятелей и черты."
+            }
+        ])
+
+        return answer
+
+    def is_style(self, text):
+        answer = self.gpt.sync_prompt([
+            {
+                "role": "system",
+                "text": 'Ты разбираешься в истории искусств. Ты отвечаешь на вопрос только "да" или "нет".'
+            },
+            {
+                "role": "user",
+                "text": f'Существует ли направление в живописи "{text}"? Ответь да или нет.'
+            }
+        ], max_tokens = 10)
+
+        return 'да' in answer or 'Да' in answer
--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,5 +1,6 @@
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from hist_engine import HistEngine
 
 start_msg = """Это бот, который генерирует конспекты по истории
 Просто введите тему билета"""
@@ -8,38 +9,14 @@ class TelegramBot:
     def __init__(self, token, gpt):
         self.app = Application.builder().token(token).build()
         self.app.add_handler(CommandHandler("start", self.start))
-        self.app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.gen_answer))
-        self.gpt = gpt
+        self.app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_msg))
+        self.hist_engine = HistEngine(gpt)
         self.app.run_polling(allowed_updates=Update.ALL_TYPES)
 
-    async def gen_answer(self, update, context):
-        print(update.message.text)
-        answer = await self.gpt.async_prompt([
-            {
-                "role": "system",
-                "text": f"Ты хорошо знаешь историю, говоришь только про историю."
-            },
-            {
-                "role": "user",
-                "text": f"Сгенерируй краткий конспект по истории по теме {update.message.text}. Назови основные даты, деятелей и черты."
-            }
-        ])
+    async def handle_msg(self, update, context):
+        konspekti = await self.hist_engine.gen_konspekti(update.message.text)
 
-        await update.message.reply_text(answer.replace("**", "*"), parse_mode="markdown")
-
-    def is_style(self, text):
-        answer = self.gpt.sync_prompt([
-            {
-                "role": "system",
-                "text": 'Ты разбираешься в истории искусств. Ты отвечаешь на вопрос только "да" или "нет".'
-            },
-            {
-                "role": "user",
-                "text": f'Существует ли направление в живописи "{text}"? Ответь да или нет.'
-            }
-        ], max_tokens = 10)
-
-        return 'да' in answer or 'Да' in answer
+        await update.message.reply_text(konspekti.replace("**", "*"), parse_mode="markdown")
 
     async def start(self, update, context):
         await update.message.reply_text(start_msg, parse_mode="markdown")
