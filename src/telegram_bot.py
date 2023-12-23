05008e0aa6411c77fcab568a1dbfaf90e042c784
koldun <eg.sharypov@yandex.ru>
2023-12-23 03:30:47 +0300

    Savely zadumalsya...

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -29,7 +29,9 @@ class TelegramBot:
             return
         chat = self.chat_db.get(update.message.chat_id)
         if chat.state == ChatState.MENU:
+            zadumalsya = await update.message.reply_text("Савелий задумался...")
             answer = await self.konspekti_engine.gen_konspekt(" ".join(context.args))
+            await zadumalsya.delete()
             await update.message.reply_text(answer.replace("**", "*"), parse_mode="markdown")
         else:
             await update.message.reply_text("Сначала закончите квиз")
@@ -41,6 +43,7 @@ class TelegramBot:
         if chat.state == ChatState.MENU:
             await update.message.reply_text("Введи команду")
         elif chat.state == ChatState.QUIZ:
+            zadumalsya = await update.message.reply_text("Савелий задумался...")
             review, next_question = await chat.quiz.answer(msg)
 
             await update.message.reply_text(review)
@@ -51,6 +54,7 @@ class TelegramBot:
                 chat.quiz = None
             else:
                 await update.message.reply_text(f"Вопрос {5 - len(chat.quiz.questions)}. {next_question.text}")
+            await zadumalsya.delete()
 
 
     async def start(self, update, context):
@@ -68,9 +72,11 @@ class TelegramBot:
         if len(context.args) == 0:
             await update.message.reply_text("Использование: /quiz <тема_квиза>")
             return
+        zadumalsya = await update.message.reply_text("Савелий задумался...")
         chat = self.chat_db.get(update.message.chat_id)
         await update.message.reply_text("Начало квиза")
         chat.state = ChatState.QUIZ
         chat.quiz = Quiz(self.gpt, " ".join(context.args))
         first_question = await chat.quiz.begin()
         await update.message.reply_text(f"Вопрос {5 - len(chat.quiz.questions)}. {first_question}")
+        await zadumalsya.delete()
